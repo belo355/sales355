7cc722e4df3bf277e817a98e65acd53e5aba1278
belo355 <edilson.belo@live.com>
2021-07-21 22:38:05 -0300

    initial commit files

---
+++ src/main/java/com/company/sales355/Sales355Application.java
@@ -0,0 +1,13 @@
+package com.company.sales355;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sales355Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sales355Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/sales355/Sales355ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.sales355;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sales355ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
